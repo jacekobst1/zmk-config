58d7b0b552a763ed6fd6692eaf55bc456cac6c1d
Jacek Obst <jacekobst1@gmail.com>
2025-09-19 21:01:21 +0200

    Paste my own config and keymap

--- config/hardware_layouts/34-keys.h
+++ config/hardware_layouts/34-keys.h
@@ -5,7 +5,8 @@
   │ 10  11  12  13  14 │ 15  16  17  18  19 │ │ LM4 LM3 LM2 LM1 LM0 │ RM0 RM1 RM2 RM3 RM4 │
   │ 20  21  22  23  24 │ 25  26  27  28  29 │ │ LB4 LB3 LB2 LB1 LB0 │ RB0 RB1 RB2 RB3 RB4 │
   ╰───────────╮ 30  31 │ 32  33 ╭───────────╯ ╰───────────╮ LH1 LH0 │ RH0 RH1 ╭───────────╯
-              ╰────────┴────────╯                         ╰─────────┴─────────╯             */
+              ╰────────┴────────╯                         ╰─────────┴─────────╯
+*/
 
 #pragma once
 
@@ -58,14 +59,3 @@
 #define RTROW  RT0 RT1 RT2 RT3 RT4                                          // left-hand-middle-row keys
 #define RMROW  RM0 RM1 RM2 RM3 RM4                                          // left-hand-middle-row keys
 #define THUMBS     LH1 LH0 RH0 RH1                                          // thumb keys
-
-#define LAYER_FROM36( \
-    k00, k01, k02, k03, k04, k05, k06, k07, k08, k09, \
-    k10, k11, k12, k13, k14, k15, k16, k17, k18, k19, \
-    k20, k21, k22, k23, k24, k25, k26, k27, k28, k29, \
-              k30, k31, k32, k33, k34, k35            \
-) \
-    k00  k01  k02  k03  k04  k05  k06  k07  k08  k09  \
-    k10  k11  k12  k13  k14  k15  k16  k17  k18  k19  \
-    k20  k21  k22  k23  k24  k25  k26  k27  k28  k29  \
-                   k31  k32  k33  k34
